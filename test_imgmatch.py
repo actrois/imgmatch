b3164be50d1e0b6e7a21e1b1839de58dfa8aefee
actrois <astrico.aulia@gmail.com>
2017-12-04 13:18:52 +0700

    Edit test folder name

--- test_imgmatch.py
+++ test_imgmatch.py
@@ -2,11 +2,11 @@ import cv2
 import imgmatch
 
 def test_duplicate():
-	img1 = cv2.imread('test/just_donwloaded_file.jpg') 
-	img2 = cv2.imread('test/selfie.jpg')
+	img1 = cv2.imread('test_images/just_donwloaded_file.jpg') 
+	img2 = cv2.imread('test_images/selfie.jpg')
 	assert imgmatch.is_duplicate(img1,img2) == True
 
 def test_not_duplicate():
-	img1 = cv2.imread('test/just_donwloaded_file.jpg') 
-	img2 = cv2.imread('test/unrelated.jpg')
+	img1 = cv2.imread('test_images/just_donwloaded_file.jpg') 
+	img2 = cv2.imread('test_images/unrelated.jpg')
 	assert imgmatch.is_duplicate(img1,img2) == False
